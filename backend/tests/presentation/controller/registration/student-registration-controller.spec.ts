c1369015b0bd15c2c32e466cbdaecf41c59267d1
RicardoEBarros <169519@upf.br>
2025-02-23 13:03:00 -0300

    test(backend): ensure StudentRegistrationController calls AddNewRegistration with correct values

--- backend/tests/presentation/controller/registration/student-registration-controller.spec.ts
+++ backend/tests/presentation/controller/registration/student-registration-controller.spec.ts
@@ -8,8 +8,8 @@ describe("Student Registration Suíte", () => {
   test("Should call Validator with correct values", async () => {
 
     const { sut, validatorStub } = makeStudentRegistrationControllerFactory()
-    const validateSpy = jest.spyOn(validatorStub, 'validate')
-    const httpRequest = { body: StudentRegistrationObjectMother.valid() }
+    const validateSpy = jest.spyOn(validatorStub, "validate")
+    const httpRequest = { body: StudentRegistrationObjectMother.withoutRaField() }
     await sut.handle(httpRequest)
 
     expect(validateSpy).toHaveBeenNthCalledWith(1, httpRequest.body)
@@ -19,8 +19,8 @@ describe("Student Registration Suíte", () => {
   test("Should return 400 if Validator fails", async () => {
 
     const { sut, validatorStub } = makeStudentRegistrationControllerFactory()
-    jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(new Error())
-    const httpRequest = { body: StudentRegistrationObjectMother.valid() }
+    jest.spyOn(validatorStub, "validate").mockReturnValueOnce(new Error())
+    const httpRequest = { body: StudentRegistrationObjectMother.withoutRaField() }
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new Error()))
@@ -30,16 +30,26 @@ describe("Student Registration Suíte", () => {
   test("Should return 500 if Validator throws", async () => {
 
     const { sut, validatorStub } = makeStudentRegistrationControllerFactory()
-    jest.spyOn(validatorStub, 'validate').mockImplementationOnce(() => { throw new Error() })
-    const httpRequest = { body: StudentRegistrationObjectMother.valid() }
+    jest.spyOn(validatorStub, "validate").mockImplementationOnce(() => { throw new Error() })
+    const httpRequest = { body: StudentRegistrationObjectMother.withoutRaField() }
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(internalServerError())
 
   })
 
-  test.todo("Should call AddRegistration with correct values")
-  test.todo("Shouldn't throws an error if AddRegistration throws")
+  test("Should call AddNewRegistration with correct values", async () => {
+
+    const { sut, addNewRegistrationStub } = makeStudentRegistrationControllerFactory()
+    const addSpy = jest.spyOn(addNewRegistrationStub, "add")
+    const httpRequest = { body: StudentRegistrationObjectMother.withoutRaField() }
+    await sut.handle(httpRequest)
+
+    expect(addSpy).toHaveBeenNthCalledWith(1, httpRequest.body)
+
+  })
+
+  test.todo("Shouldn't throws an error if AddNewRegistration throws")
   test.todo("Should return 200 on success")
 
 })
