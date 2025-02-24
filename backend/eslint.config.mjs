becc308402641c074ba0700341fa58dc0f37f7c2
RicardoEBarros <169519@upf.br>
2025-02-24 06:13:46 -0300

    chore(backend): fix eslint configs

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,17 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import stylisticJs from '@stylistic/eslint-plugin'
+import stylisticTs from '@stylistic/eslint-plugin-ts'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["./backend/src/**/*.ts", "./backend/tests/**/*.ts"],
+    files: ["src/**/*.{js,mjs,cjs,ts}", "tests/**/*.ts"],
     ignores: ["node_modules", "dist", "coverage"],
     plugins: {
-      '@stylistic/js': stylisticJs
+      '@stylistic/ts': stylisticTs
     },
     rules: {
       "semi": ["error", "never"],
@@ -22,11 +22,11 @@ export default [
       "indent": ["error", 2],
       "prefer-const": "warn",
       "quotes": ["error", "double"],
+      "eol-last": ["error", "always"],
       "arrow-spacing": ["error", { before: true, after: true }],
       "@typescript-eslint/strict-boolean-expressions": "off",
-      "@stylistic/js/function-call-spacing": ["error", "never"],
-      "@stylistic/js/array-bracket-spacing": ["error", "always"],
-      "@stylistic/js/eol-last": ["error", "always"],
+      "@stylistic/ts/function-call-spacing": ["error", "never"],
+      "@stylistic/ts/brace-style": ["error", "1tbs", { "allowSingleLine": true }],
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off"
     },
@@ -35,7 +35,7 @@ export default [
     languageOptions: { 
       globals: globals.browser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: 'tsconfig-eslint.json',
         tsConfigRootDir: '.'
       }
     }
